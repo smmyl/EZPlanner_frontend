5b19b224d656c867ebd2fee878acec3892ca9052
Sammy <sammy.liaop@gmail.com>
2023-04-26 06:14:16 -0400

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import Login from './components/Login'
 
-function App() {
+const App = () => {
+  const [home, setHome] = useState(true)
+  const [profile, setProfile] = useState(false)
+  const [login, setLogin] = useState(false)
+
+  const homeToggle = () => {
+    setHome(true)
+    setProfile(false)
+    setLogin(false)
+  }
+  const profileToggle = () => {
+    setHome(false)
+    setProfile(true)
+    setLogin(false)
+  }
+  const loginToggle = () => {
+    setHome(false)
+    setProfile(false)
+    setLogin(true)
+  }
+
+  useEffect(() => {
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {home ?
+      <Home
+        homeToggle={homeToggle}
+        profileToggle={profileToggle}
+        loginToggle={loginToggle}
+      />
+      :profile ?
+      <Profile
+        homeToggle={homeToggle}
+        profileToggle={profileToggle}
+        loginToggle={loginToggle}
+      />
+      :login ?
+      <Login
+        homeToggle={homeToggle}
+        profileToggle={profileToggle}
+        loginToggle={loginToggle}
+      />
+      :
+      <>
+      </>
+      }
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+
+const Login = (props) => {
+    return (
+        <>
+            <ul class='flex'>
+                <li class='mr-6'>
+                    <a class='text-blue-500 hover:text-blue-800 cursor-pointer' onClick={props.homeToggle}>Home</a>
+                </li>
+                <li class='mr-6'>
+                    <a class='text-blue-500 hover:text-blue-800 cursor-pointer' onClick={props.profileToggle}>Profile</a>
+                </li>
+                <li class='mr-6'>
+                    <a class='text-blue-800 hover:text-blue-800 cursor-pointer' onClick={props.loginToggle}>Sign In / Up</a>
+                </li>
+            </ul>
+            <h1>Login</h1>
+        </>
+    )
+}
+
+export default Login;
